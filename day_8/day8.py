1531aa4f894033ee518502e9f41b96a3f1a2fb70
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-08 16:18:34 +0000

    Completed Day 8 Part 2 with a brute force solution.

--- day_8/day8.py
+++ day_8/day8.py
@@ -8,9 +8,9 @@ def simulateCode(program) -> (int, int):
     lines = [0 for i in range(len(program))]
     globalCounter = 0
     while True:
-        line = program[lineIdx].strip()
         if lineIdx >= len(program):
             return (0, globalCounter)
+        line = program[lineIdx].strip()
         if lines[lineIdx]:
             return (-1, globalCounter)
         lines[lineIdx] = 1
@@ -24,8 +24,21 @@ def simulateCode(program) -> (int, int):
         elif "jmp" in line:
             lineIdx += parseNumber(line)
 
+def fixProgram(program) -> int:
+    for i in range(len(program)):
+        attempt = program.copy()
+        if "jmp" in attempt[i]:
+            attempt[i] = "nop" + attempt[i][3:]
+        if "nop" in program[i]:
+            attempt[i] = "jmp" + attempt[i][3:]
+        success, result = simulateCode(attempt)
+        if success is 0:
+            return result
+
 
 with open('input.in', 'r') as f:
-    lines = f.readlines()
-    success, result = simulateCode(lines)
+    program = f.readlines()
+    success, result = simulateCode(program)
     print(f"Program terminated with: {success}. The final value was: {result}")
+    result = fixProgram(program)
+    print(f"Program terminated with: 0. The final value was: {result}")
