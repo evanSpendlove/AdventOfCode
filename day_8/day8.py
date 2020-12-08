a09e1c0f3067eed278c071ee0a91c866ffd9313c
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-08 18:40:08 +0000

    Completed Day 8 with iterative solution

--- day_8/day8.py
+++ day_8/day8.py
@@ -53,6 +53,28 @@ def simulateLine(lineIdx, program):
     line = program[lineIdx].strip()
     return lineIdx + 1 if "jmp" not in line else lineIdx + parseNumber(line)
 
+def generateWinningStatesIter(program):
+    winning = [None for i in range(len(program))]
+    for i in range(len(program)):
+        cycleResult = []
+        lineIdx = i
+        while len(cycleResult) is 0:
+            if lineIdx >= len(program):
+                cycleResult.append(1)
+                break
+            if winning[lineIdx] is not None:
+                if len(winning[lineIdx]) is 0: 
+                    winning[lineIdx] = cycleResult
+                    cycleResult.append(-1)
+                    break
+                if len(winning[lineIdx]) > 0:
+                    cycleResult.append(winning[lineIdx][0])
+                    break
+            winning[lineIdx] = cycleResult
+            lineIdx = simulateLine(lineIdx, program)
+    return winning
+
+
 def generateWinningStates(lineIdx, program, winning, visited):
     if lineIdx >= len(program):
         return 1
@@ -71,6 +93,8 @@ def verifyWinning(program):
     winning_clever = [0 for i in range(len(program))]
     visited = [False for i in range(len(program))]
     computeWinningStates(program, winning_clever, visited)
+    winning_super_clever = generateWinningStatesIter(program)
+    print(winning_super_clever)
 
     winning_dumb = [0 for i in range(len(program))]
     for i in range(len(program)):
@@ -78,13 +102,40 @@ def verifyWinning(program):
 
     brokenCounter = 0
     for i in range(len(winning_dumb)):
-        brokenCounter += 1 if winning_dumb[i] != winning_clever[i] else 0
+        brokenCounter += 1 if winning_dumb[i] != winning_super_clever[i][0] else 0
+        print(f"{winning_dumb[i]} == {winning_super_clever[i]}")
     print(f"verifyWinning.Broken Count: {brokenCounter}")
-
 
 def computeWinningStates(program, winning, visited):
     for i in range(len(program)):
         generateWinningStates(i, program, winning, visited)
+
+def fixProgramLinearSmart(program) -> int:
+    start = current_milli_time()
+    winning = generateWinningStatesIter(program)
+    end = current_milli_time()
+    print(f"ITER: Time taken to generate winning states: {end-start}")
+    print(len(winning))
+
+    start = current_milli_time()
+    for i in range(len(program)):
+        if "jmp" not in program[i] and "nop" not in program[i]:
+            continue
+        offset = 1 if "jmp" in program[i] else parseNumber(program[i])
+        if i + offset >= len(winning):
+            print("WTFFF" + str((i + offset)))
+        if winning[i + offset][0] == 1:
+            attempt = program.copy()
+            if "jmp" in attempt[i]:
+                attempt[i] = "nop" + attempt[i][3:]
+            if "nop" in program[i]:
+                attempt[i] = "jmp" + attempt[i][3:]
+            success, result = simulateCode(0, attempt)
+            if success is 1:
+                end = current_milli_time()
+                print(f"ITER: Time taken to fix program linear: {end-start}")
+                return result
+
 
 def fixProgramLinear(program) -> int:
     winning = [0 for i in range(len(program))]
@@ -93,6 +144,7 @@ def fixProgramLinear(program) -> int:
     computeWinningStates(program, winning, visited)
     end = current_milli_time()
     print(f"Time taken to generate winning states: {end-start}")
+    print(f"Length of winning {len(winning)}")
 
     start = current_milli_time()
     for i in range(len(program)):
@@ -114,17 +166,21 @@ def fixProgramLinear(program) -> int:
 with open('input.in', 'r') as f:
     program = f.readlines()
     success, result = simulateCode(0, program)
+    verifyWinning(program)
     print(f"Program terminated with: {success}. The final value was: {result}")
     print(f"Program terminated with: 0. The final value was: {fixProgram(program)}")
     print(f"Program terminated with: 0. The final value was: {fixProgramLinear(program)}")
-    # verifyWinning(program)
-   
+    print(f"Program terminated with: 0. The final value was: {fixProgramLinearSmart(program)}")
+
     speedTest = True
     if speedTest:
         bruteFunc = wrapper(fixProgram, program)
         optimalFunc = wrapper(fixProgramLinear, program)
+        iterFunc = wrapper(fixProgramLinearSmart, program)
         bruteTime = timeit.timeit(bruteFunc, number=1)
         optimalTime = timeit.timeit(optimalFunc, number=1)
+        iterTime = timeit.timeit(iterFunc, number=1)
         print(f"Time taken for brute force approach: {bruteTime}")
         print(f"Time taken for optimal approach: {optimalTime}")
+        print(f"Time taken for iterative approach: {iterTime}")
 
