07fc9d070c6755eb498dfd01f933d8e9f9780963
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-09 12:33:27 +0000

    Day 9: Added O(N) sliding window solution.

--- day_9/day9.py
+++ day_9/day9.py
@@ -6,6 +6,22 @@ def findInvalidXMAS(nums):
     idx = [not isValidXMAS(i, nums) for i in range(25, len(nums))].index(True) + 25
     return nums[idx]
 
+# O(N) worst case
+def slidingXMAS(target, nums):
+    start, end = 0, 1
+    curSum = nums[start] + nums[end]
+    while start < len(nums) and end < len(nums):
+        if curSum == target:
+            return min(nums[start:end+1]) + max(nums[start:end+1])
+        if curSum < target:
+            end += 1
+            curSum += nums[end] if end < len(nums) else 0
+        if curSum > target:
+            curSum -= nums[start]
+            start += 1
+    return -1
+
+# O(N^2) worst case
 def findXMASWeakness(target, nums):
     for i in range(len(nums)):
         curSum = nums[i]
@@ -22,3 +38,4 @@ with open('input.in', 'r') as f:
     invalid = findInvalidXMAS(nums)
     print(f"P1: Invalid value: {invalid}")
     print(f"P2: XMAS Encrpytion Weakness: {findXMASWeakness(invalid, nums)}")
+    print(f"P2: XMAS Sliding Window: {slidingXMAS(invalid, nums)}")
