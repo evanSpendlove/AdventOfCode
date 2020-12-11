051e42d0e68084d8aa6af17210fd1ce537afe681
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-11 15:31:55 +0000

    Completed Day 11 Part 2

--- day_11/day11.py
+++ day_11/day11.py
@@ -1,4 +1,4 @@
-def adjacentOcc(i, j, floorMap) -> int:
+def countAdj(i, j, floorMap) -> int:
     offsets = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
     count = 0
     for idx in range(len(offsets)):
@@ -8,25 +8,39 @@ def adjacentOcc(i, j, floorMap) -> int:
             count += 1 if floorMap[x][y] == '#' else 0
     return count
 
+def advancedAdj(i, j, floorMap) -> int:
+    offsets = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    count = 0
+    for idx in range(len(offsets)):
+        found = False
+        dx, dy = offsets[idx]
+        x, y = i + dx, j + dy
+        while not found and (x >= 0 and x < len(floorMap)) and (y >= 0 and y < len(floorMap[0])):
+            found = floorMap[x][y] != '.'
+            count += 1 if floorMap[x][y] == '#' and found else 0
+            x, y = x + dx, y + dy
+    return count
+
 def countSeats(floorMap) -> int:
     return sum([val == '#' for row in floorMap for val in row])
 
-def simulateSeats(floorMap) -> int:
+def simulateSeats(floorMap, adjFunc, upperLimit) -> bool:
     newMap = [row[:] for row in floorMap]
     changed = False
     for i in range(len(floorMap)):
         for j in range(len(floorMap[0])):
-            if floorMap[i][j] == 'L' and adjacentOcc(i, j, floorMap) == 0:
+            if floorMap[i][j] == 'L' and adjFunc(i, j, floorMap) == 0:
                 newMap[i][j] = "#"
                 changed = True
-            if floorMap[i][j] == '#' and adjacentOcc(i, j, floorMap) >= 4:
+            if floorMap[i][j] == '#' and adjFunc(i, j, floorMap) >= upperLimit:
                 newMap[i][j] = "L"
                 changed = True
     return newMap, changed
 
 with open('input.in', 'r') as f:
-    floorMap = [list(i) for i in f.readlines()]
-    stableMap, changed = simulateSeats(floorMap)
-    while changed:
-        stableMap, changed = simulateSeats(stableMap)
-    print(f"Changed: {changed}, Number of seats: {countSeats(stableMap)}")
+    floorMap = [list(i.strip()) for i in f.readlines()]
+    parts = [[countAdj, 4], [advancedAdj, 5]]
+    for p in parts:
+        seats, changed = simulateSeats(floorMap, p[0], p[1])
+        while changed: seats, changed = simulateSeats(seats, p[0], p[1])
+        print(f"P{parts.index(p)+1}: Number of seats: {countSeats(seats)}")
