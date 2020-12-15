a1c6b2eb576a931429eb9ad22d454f953fde3fdd
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-15 13:42:12 +0000

    Reduced Day 15 to 10 lines.

--- day_15/day15.py
+++ day_15/day15.py
@@ -1,26 +1,11 @@
-def memoryGame(nums, end) -> int:
-    lastSpoken = {num: i+1 for i, num in enumerate(nums[:-1])}
-    curNum = nums[-1]
-    for turn in range(len(nums), end):
-        prevNum = curNum
-        curNum = turn - lastSpoken.get(curNum, turn)
-        lastSpoken[prevNum] = turn
-    return curNum
-
+def memoryGame(numbers, end) -> int:
+    lastSpoken = {n: i+1 for i, n in enumerate(numbers[:-1])}
+    num = numbers[-1]
+    for turn in range(len(numbers), end):
+        lastSpoken[num], num = turn, turn - lastSpoken.get(num, turn)
+    return num
 
 with open('input.in', 'r') as f:
     lines = [int(i) for i in f.read().strip().split(',')]
-    tests = [
-                [0, 3, 6],
-                [1, 3, 2],
-                [2, 1, 3],
-                [1, 2, 3],
-                [2, 3, 1],
-                [3, 2, 1],
-                [3, 1, 2]
-            ]
-    outputs = [436, 1, 10, 27, 78, 438, 1836]
-    for i, t in enumerate(tests):
-        assert(outputs[i] == memoryGame(t, 2020))
     print(memoryGame(lines, 2020))
     print(memoryGame(lines, 30000000))
