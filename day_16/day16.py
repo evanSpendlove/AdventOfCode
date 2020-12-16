f5ee6e0183576b89ca18d2353552029fa96e80ad
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-16 23:18:52 +0000

    Polished Day 16 - Part 2

--- day_16/day16.py
+++ day_16/day16.py
@@ -1,15 +1,6 @@
-# RREF - should've transposed before doing RREF...
+import functools as ft
 
 # ------------- Identifying Fields-------------
-def basicRREF(matrix):
-    # counts = list(ruleCounts.values())
-    for r in range(len(matrix)):
-        for c in range(len(matrix[0])):
-            matrix[r][c] -= 190
-
-    for r in matrix:
-        print(r)
-
 def removeInvalidTickets(tickets, rules):
     return [t for t in tickets if validateTicket(t, rules) == 0]
 
@@ -21,53 +12,29 @@ def identifyFields(tickets, rules):
             for idx, field in enumerate(t):
                 if any([withinRange(field, i) for i in rules[r]]):
                     count[idx] += 1
-    for _, count in ruleCounts.items():
-        for c in range(len(count)):
-            count[c] -= 190
 
-    for r in ruleCounts:
-        print(ruleCounts[r])
-
+    ruleCounts = {rule:[c - 190 for c in ruleCounts[rule]] for rule in ruleCounts.keys()}
     fields = ["" for i in range(len(rules))]
     for i in range(len(rules)):
         for rule, count in ruleCounts.items():
             if sum(count) == 1:
                 position = count.index(1)
                 fields[position] = rule
-                print(rule, position)
                 for r in ruleCounts:
                     ruleCounts[r][position] = 0
     return fields
-    # Rows = rules ;; Cols = tickets
-    # counts = list(ruleCounts.values())
-    # for r in range(len(counts)):
-        # for c in range(len(counts[0])):
-            # counts[r][c] -= 190
-    # for r in counts:
-        # print(r)
-    # fields = {}
-    # for row in range(len(counts)):
-        # possibilities = sum([counts[row][col] for col in range(len(counts[0]))])
-        # if possibilities == 1:
-        # print(possibilities)
 
 def productOfDepartures(myTicket, fields):
-    prod = 1
-    for idx, f in enumerate(fields):
-        if 'departure' in f:
-            prod *= myTicket[idx]
-    return prod
+    vals = [myTicket[idx] for idx, f in enumerate(fields) if 'departure' in f]
+    return ft.reduce(lambda x,y: x*y, vals)
 
 # ------------- Validating Tickets -------------
 
 def withinRange(field, i):
-    return 1 if field >= i[0] and field <= i[1] else 0
+    return True if field >= i[0] and field <= i[1] else False
 
 def isValidField(field, rules) -> int:
-    if any([withinRange(field, i) for interval in rules.values() for i in interval]):
-        return 0
-    else:
-        return field
+    return 0 if any([withinRange(field, interval) for rule in rules.values() for interval in rule]) else field
 
 def validateTicket(ticket, rules):
     return sum([isValidField(field, rules) for field in ticket])
@@ -95,7 +62,8 @@ def parseSections(sections):
 with open('input.in', 'r') as f:
     sections = f.read().strip().split("\n\n")
     rules, myTicket, otherTickets = parseSections(sections)
-    print(sum(validateTicket(t, rules) for t in otherTickets))
+    print(f"P1: Ticket scanning error rate: {sum(validateTicket(t, rules) for t in otherTickets)}")
+
     otherTickets.append(myTicket)
     fields = identifyFields(otherTickets, rules)
-    print(productOfDepartures(myTicket, fields))
+    print(f"P2: Magic number: {productOfDepartures(myTicket, fields)}")
