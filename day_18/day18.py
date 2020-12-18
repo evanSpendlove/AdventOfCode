edbab6557e1e29949d0db58469dae1d015ec16d0
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-18 19:38:22 +0000

    Completed Day 18 - moved to a clever solution using eval

--- day_18/day18.py
+++ day_18/day18.py
@@ -1,27 +1,24 @@
-def indexOfMatchingParens(equation):
-    count = 0
-    for i, c in enumerate(equation):
-        if c == ')': count += 1
-        if c == '(': count -= 1
-        if count == 0: return i
+import re
 
-def computeEquation(equation):
-    calc = {'+':lambda x,y: x + y, '*': lambda x,y: x*y}
-    if len(equation) == 1:
-        return int(equation[0])
-    if equation[0] == ')':
-        end = indexOfMatchingParens(equation)
-        arg1 =  computeEquation(equation[1:end])
-        if len(equation[end:]) < 3:
-            return arg1
-        operator = equation[end + 2]
-        arg2 = computeEquation(equation[end + 4:])
-        return  calc[operator](arg1, arg2)
-    else:
-        arg1 = int(equation[0])
-        operator = equation[2]
-        return calc[operator](arg1, computeEquation(equation[4:]))
+class num:
+    def __init__(self, val):
+        self.val = val
+    def __mul__(self, other):
+        return num(self.val * other.val)
+    def __truediv__(self, other):
+        return num(self.val * other.val)
+    def __pow__(self, other):
+        return num(self.val + other.val)
+
+def evaluate(equation):
+    v = re.sub(r'(\d+)', r'num(\1)', equation).replace('+','/')
+    return eval(v).val
+
+def evaluate2(equation):
+    v = re.sub(r'(\d+)', r'num(\1)', equation).replace('+','**')
+    return eval(v).val
 
 with open('input.in', 'r') as f:
-    equations = [i[::-1] for i in f.read().strip().split('\n')]
-    print(f"P1: Sum of results: {sum(list(map(computeEquation, equations)))}")
+    equations = f.read().strip().split('\n')
+    print(f"P1: {sum([evaluate(e) for e in equations])}")
+    print(f"P2: {sum([evaluate2(e) for e in equations])}")
