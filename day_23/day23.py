33051ac0c0ef185d0373e44ae05846986c239ca7
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-26 12:11:07 +0000

    Completed Day 23 - Part 2

--- day_23/day23.py
+++ day_23/day23.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
 def findDestinationIdx(targetNum, pickedUp, n):
     for i in range(4):
         if targetNum < 1:
@@ -6,35 +12,68 @@ def findDestinationIdx(targetNum, pickedUp, n):
             targetNum -= 1
         else:
             return targetNum
+def scoreP1(cups, nodes):
+    cur = nodes[1].next
+    vals = []
+    while cur != nodes[1]:
+        vals.append(str(cur.val))
+        cur = cur.next
+    return ''.join(vals)
 
-def scoreP1(cups):
-    return ''.join(map(str, cups[cups.index(1)+1:] + cups[:cups.index(1)]))
+def scoreP2(cups, nodes):
+    return nodes[1].next.val * nodes[1].next.next.val
 
-def scoreP2(cups):
-    s = cups.index(1)
-    return cups[(s+1) % len(cups)] * cups[(s+2) % len(cups)]
+def pickThree(cur):
+    start, pickedVals = cur, []
+    for i in range(3):
+        pickedVals.append(cur.val)
+        cur = cur.next
+    prev = cur.prev
+    start.prev.next = cur
+    cur.prev = start.prev
+    prev.next = None
+    return start, pickedVals
 
-# Expensive operations:
-# cups.index()
-# cups = cups[] + p + cups[] --> changed to .insert()
-# cups.remove()
+def insertAtDestination(destination, pickedUp):
+    end = destination.next
+    destination.next = pickedUp
+    pickedUp.prev = destination
+    for i in range(2): pickedUp = pickedUp.next
+    pickedUp.next = end
+    end.prev = pickedUp
 
-def moveCups(cups, score):
-    cur, n = 0, len(cups)
-    for i in range(1000):
-        curNum = cups[cur]
-        pickedUp = cups[cur+1:cur+4]
-        if cur + 3 >= n:
-            pickedUp += cups[:(cur + 4 - n)]
-        for p in pickedUp: cups.remove(p)
-        destination = cups.index(findDestinationIdx(curNum - 1, pickedUp, n))
-        for i in range(3):
-            cups.insert(destination+1+i, pickedUp[i])
-        cur = (cups.index(curNum) + 1) % (len(cups))
-    return score(cups)
+def moveCups(cups, nodes, turnCount, score, n):
+    cur = cups
+    for i in range(turnCount):
+        curVal = cur.val
+        pickedUp, pickedVals = pickThree(cur.next)
+        destination = nodes[(findDestinationIdx(curVal - 1, pickedVals, n))]
+        insertAtDestination(destination, pickedUp)
+        cur = nodes[curVal].next
+    return score(cups, nodes)
+
+def createList(vals):
+    head = Node(vals[0])
+    prev = head
+    nodes = {cups[0]: head}
+    for i in range(1, len(cups)):
+        # Create node and dict entry
+        cur = Node(cups[i])
+        nodes[cups[i]] = cur
+        # Doubly link
+        prev.next = cur
+        cur.prev = prev
+        # Update previous node
+        prev = cur
+    # Make it circular
+    head.prev = cur
+    cur.next = head
+    return head, nodes
 
 with open('input.in', 'r') as f:
     cups = [int(c) for c in f.read().strip()]
-    # print(moveCups([c for c in cups], scoreP1))
+    p1_head, p1_nodes = createList(cups)
+    print(moveCups(p1_head, p1_nodes, 100, scoreP1, len(cups)))
     for i in range(len(cups)+1, 1000001): cups.append(i)
-    print(moveCups(cups, scoreP2))
+    p2_head, p2_nodes = createList(cups)
+    print(moveCups(p2_head, p2_nodes, 10000000, scoreP2, len(cups)))
