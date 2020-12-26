9bc389f15989fac972effd4dffca6e07a828fc16
evanSpendlove <evan.spendlove@ucdconnect.ie>
2020-12-26 14:59:20 +0000

    Completed Day 24 - Part 2

--- day_24/day24.py
+++ day_24/day24.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def parseTile(tile):
     directions = {
             "e":(1, 0), "w":(-1, 0),
@@ -21,8 +23,30 @@ def flipTiles(tiles):
             flipped.remove(flip)
         else:
             flipped.add(flip)
+    return flipped
+
+def neighbours(point):
+    offsets = [(1, 0), (-1, 0), (1, 1), (0, 1), (0, -1), (-1, -1)]
+    return [(tuple(map(sum, zip(point, o)))) for o in offsets]
+
+def countNeighbours(flipped, point):
+    return sum([1 for n in neighbours(point) if n in flipped])
+
+def simulateArt(flipped):
+    for i in range(100):
+        basis = deepcopy(flipped)
+        for t in basis:
+            pointsToExplore = [t] + neighbours(t)
+            for p in pointsToExplore:
+                count = countNeighbours(basis, p)
+                if count == 0 or count > 2 and p in basis and p in flipped:
+                    flipped.remove(p)
+                if count == 2 and p not in basis:
+                    flipped.add(p)
     return len(flipped)
 
 with open('input.in', 'r') as f:
     tiles = f.read().strip().split('\n')
-    print(flipTiles(tiles))
+    flipped = flipTiles(tiles)
+    print(f"P1: {len(flipped)}")
+    print(f"P2: {simulateArt(flipped)}")
